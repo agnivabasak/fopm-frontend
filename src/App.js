8442e0eaa01173899d135c5572e928d3e03c45b2
Agniva Basak <agnivabasak1@gmail.com>
2021-03-17 19:20:28 +0530

    landing page created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import LandingScreen from "./screens/LandingScreen/LandingScreen"; 
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
+const AppRouter = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={LandingScreen} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
-export default App;
+export default AppRouter;
